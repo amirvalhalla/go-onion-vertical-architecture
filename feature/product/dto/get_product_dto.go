eec1618723b6bde924b9bae733ba6f2460703fc3
amirvalhalla <amirvalhalla@proton.me>
2023-11-25 22:30:38 +0330

    fix: add id field to get product dto

--- feature/product/dto/get_product_dto.go
+++ feature/product/dto/get_product_dto.go
@@ -1,7 +1,10 @@
 package product
 
+import "github.com/google/uuid"
+
 type GetDto struct {
-	Name           string `json:"name"`
-	AvailableCount int    `json:"availableCount"`
-	IsAvailable    bool   `json:"isAvailable"`
+	ID             uuid.UUID `json:"id"`
+	Name           string    `json:"name"`
+	AvailableCount int       `json:"availableCount"`
+	IsAvailable    bool      `json:"isAvailable"`
 }
